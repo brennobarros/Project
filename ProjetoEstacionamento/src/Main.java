bc8f94f198ce3d8e1130f0b5bcd9a6bb4abdf833
brennobarros <brenno.c31@gmail.com>
2022-03-29 21:30:51 -0300

    commit

---
+++ ProjetoEstacionamento/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import model.Usuario;
+import view.LoginView;
+//import model.Veiculo;
+
+public class Main {
+
+	public static void main(String[] args) {
+				
+		LoginView lg = new LoginView();
+		lg.login();
+	}
+
+}
